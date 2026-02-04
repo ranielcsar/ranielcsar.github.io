bb687959d625646bf9c0c0bd085f4e6dd7c96b36
Raniel <ranielcsardev@gmail.com>
2026-02-03 21:35:18 -0300

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ function renderChunks(deps: Record<string, string>) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ranielcsar.github.io/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
